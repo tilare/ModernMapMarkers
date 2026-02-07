77c43421f50c98e40aaf1923a2b036bedb2cf853
CommanderKeen34 <christianpanzer@googlemail.com>
2026-02-07 23:42:02 +0000

    updated Sturmwind POIs

--- MarkerData.lua
+++ MarkerData.lua
@@ -1,6 +1,109 @@
 -- Format: { continent, zoneID, x, y, name, type, info, Atlas ID }
 ModernMapMarkers_Points = {
     -- Kalimdor Dungeons
+    {1, 11, 0.123, 0.128, "Tiefschwarze Grotte", "dungeon", "24-32", 2},
+    {1, 25, 0.66, 0.49, "Höhlen der Zeit: Schwarzer Morast", "dungeon", "60", 1},
+    {1, 11, 0.51, 0.78, "Mondsichelhain", "dungeon", "32-38", 14},
+    {1, 12, 0.648, 0.303, "Düsterbruch – Ost", "dungeon", "55-58", 3},
+    {1, 12, 0.624, 0.249, "Düsterbruch – Nord", "dungeon", "57-60", 4},
+    {1, 12, 0.604, 0.311, "Düsterbruch – West", "dungeon", "57-60", 5},
+    {1, 4, 0.29, 0.629, "Maraudon", "dungeon", "46-55", 7},
+    {1, 21, 0.53, 0.486, "Der Flammenschlund", "dungeon", "13-18", 9},
+    {1, 2, 0.488, 0.919, "Kral der Klingenhauer", "dungeon", "37-46", 11},
+    {1, 2, 0.407, 0.873, "Hügel der Klingenhauer", "dungeon", "29-38", 10},
+    {1, 2, 0.462, 0.357, "Die Höhlen des Wehklagens", "dungeon", "17-24", 15},
+    {1, 25, 0.389, 0.184, "Zul'Farrak", "dungeon", "44-54", 16},
+	-- Kalimdor Raids
+	{1, 14, 0.207, 0.592, "Smaragdsanktum", "raid", "60", 6},
+	{1, 9, 0.53, 0.76, "Onyxias Hort", "raid", "60", 8},
+	{1, 23, 0.296, 0.960, "Ruinen von Ahn’Qiraj", "raid", "60", 12},
+	{1, 23, 0.282, 0.956, "Tempel von Ahn’Qiraj", "raid", "60", 13},
+	-- Kalimdor World Bosses
+	{1, 1, 0.535, 0.816, "Azuregos", "worldboss", "60", nil},
+	{1, 1, 0.69, 0.094, "Cla'ckora", "worldboss", "60", nil},
+	{1, 4, 0.82, 0.80, "Concavius", "worldboss", "60", nil},
+	{1, 14, 0.336, 0.398, "Vater Lykan", "worldboss", "60", nil},
+	{1, 25, 0.361, 0.762, "Ostarius", "worldboss", "60", nil},
+	{1, 11, 0.937, 0.355, "Smaragddrache – Erscheinungspunkt 1 von 4", "worldboss", "60", nil},
+	{1, 12, 0.512, 0.108, "Smaragddrache – Erscheinungspunkt 2 von 4", "worldboss", "60", nil},
+    -- Kalimdor Transport
+    {1, 7, 0.512, 0.135, "Zeppeline nach Unterstadt & Grom’gol", "zepp", "Horde", nil},  -- horde
+    {1, 7, 0.415, 0.184, "Zeppeline nach Donnerfels & Kargath", "zepp", "Horde", nil},   -- horde
+    {1, 5, 0.165, 0.230, "Zeppelin nach Orgrimmar", "zepp", "Horde", nil},  -- horde
+    {1, 8, 0.598, 0.236, "Schiff ins Dorf der Bruchhauer", "boat", "Horde", nil},  -- horde
+    {1, 2, 0.636, 0.389, "Schiff nach Beutebucht", "boat", "Neutral", nil},  -- neutral
+	{1, 6, 0.324, 0.44, "Schiff nach Sturmwind", "boat", "Allianz", nil}, -- alliance
+	{1, 6, 0.304, 0.41, "Schiff nach Alah’Thalas", "boat", "Allianz", nil},  -- alliance
+	{1, 6, 0.333, 0.399, "Schiff nach Rut’theran", "boat", "Allianz", nil}, -- alliance
+	{1, 9, 0.718, 0.566, "Schiff nach Hafen von Menethil", "boat", "Allianz", nil}, -- alliance
+	{1, 12, 0.311, 0.395, "Schiff zur Vergessenen Küste", "boat", "Allianz", nil}, -- alliance
+	{1, 12, 0.431, 0.428, "Schiff zur Sardor-Insel", "boat", "Allianz", nil}, -- alliance
+	{1, 28, 0.552, 0.949, "Schiff nach Auberdine", "boat", "Allianz", nil}, -- alliance
+    -- Eastern Kingdoms Dungeons
+    {2, 24, 0.371, 0.857, "Schwarzfelstiefen", "dungeon", "52-60", 1},
+	{2, 5, 0.328, 0.362, "Schwarzfelstiefen", "dungeon", "52-60", 1},
+    {2, 36, 0.423, 0.726, "Die Todesminen", "dungeon", "17-24", 22},
+    {2, 15, 0.30, 0.27, "Stadt von Gilneas", "dungeon", "43", 4},
+    {2, 9, 0.248, 0.337, "Gnomeregan", "dungeon", "29-38", 5},
+    {2, 5, 0.95, 0.53, "Hassschmiede Steinbruch", "dungeon", "52-60", 6},
+    {2, 13, 0.45, 0.75, "Karazhan Krypta", "dungeon", "58-60", 7},
+    {2, 5, 0.321, 0.386, "Untere Schwarzfelsspitze", "dungeon", "55-60", 8},
+	{2, 24, 0.364, 0.879, "Untere Schwarzfelsspitze", "dungeon", "55-60", 8},
+    {2, 30, 0.869, 0.323, "Scharlachrotes Kloster – Waffenkammer", "dungeon", "32-42", 12}, 
+    {2, 30, 0.862, 0.295, "Scharlachrotes Kloster – Kathedrale", "dungeon", "35-45", 13}, 
+    {2, 30, 0.839, 0.283, "Scharlachrotes Kloster – Friedhof", "dungeon", "26-36", 14},
+    {2, 30, 0.850, 0.338, "Scharlachrotes Kloster – Bibliothek", "dungeon", "29-39", 15},
+    {2, 37, 0.69, 0.74, "Scholomance", "dungeon", "58-60", 16},
+    {2, 25, 0.44, 0.67, "Burg Schattenfang", "dungeon", "22-30", 17},
+    {2, 26, 0.51, 0.675, "Das Verlies", "dungeon", "24-31", 23},
+    {2, 26, 0.63, 0.58, "Tresor von Sturmwind", "dungeon", "60", 18},
+    {2, 35, 0.29, 0.61, "Tresor von Sturmwind - Horde Eingang", "dungeon", "60", 18},
+    {2, 39, 0.31, 0.14, "Stratholme", "dungeon", "58-60", 20},
+    {2, 39, 0.47, 0.24, "Stratholme – Hintereingang", "dungeon", "58-60", 20},
+    {2, 28, 0.701, 0.55, "Der Versunkene Tempel", "dungeon", "50-60", 21},
+    {2, 38, 0.429, 0.130, "Uldaman – Haupteingang", "dungeon", "41-51", 25},
+    {2, 38, 0.657, 0.438, "Uldaman – Hintereingang", "dungeon", "41-51", 25},
+    {2, 5, 0.312, 0.365, "Obere Schwarzfelsspitze", "dungeon", "55-60", 26},
+	{2, 24, 0.355, 0.855, "Obere Schwarzfelsspitze", "dungeon", "55-60", 26},
+	{2, 27, 0.67, 0.634, "Zuflucht des Drachenmals", "dungeon", "27-33", 3},
+	{2, 4, 0.57, 0.598, "Ruine von Sturmschmied", "dungeon", "35-41", 19},
+	-- Eastern Kingdoms Raids
+	{2, 24, 0.332, 0.851, "Pechschwingenhort", "raid", "60", 2},
+	{2, 5, 0.273, 0.363, "Pechschwingenhort", "raid", "60", 2},
+	{2, 13, 0.46, 0.70, "Untere Hallen von Karazhan", "raid", "58-60", 9},
+	{2, 24, 0.336, 0.879, "Geschmolzener Kern", "raid", "60", 10},
+	{2, 5, 0.273, 0.387, "Geschmolzener Kern", "raid", "60", 10},
+	{2, 39, 0.40, 0.28, "Naxxramas", "raid", "60", 11},
+	{2, 13, 0.442, 0.719, "Turm von Karazhan", "raid", "60", 24},
+	{2, 23, 0.53, 0.172, "Zul'Gurub", "raid", "60", 27},
+	-- Eastern Kingdoms World Bosses
+	{2, 13, 0.471, 0.751, "Dunkler Plünderer von Karazhan", "worldboss", "60", nil},
+	{2, 10, 0.465, 0.357, "Smaragddrache – Erscheinungspunkt 3 von 4", "worldboss", "60", nil},
+	{2, 17, 0.632, 0.217, "Smaragddrache – Erscheinungspunkt 4 von 4", "worldboss", "60", nil},
+	{2, 33, 0.36, 0.753, "Lord Kazzak", "worldboss", "60", nil},
+	{2, 39, 0.082, 0.38, "Nerubischer Aufseher", "worldboss", "60", nil},
+	-- Eastern Kingdoms Transport
+	{2, 26, 0.694, 0.294, "Tiefenbahn nach Eisenschmiede", "tram", "Allianz", nil},  -- alliance
+	{2, 12, 0.762, 0.511, "Tiefenbahn nach Sturmwind", "tram", "Allianz", nil},  -- alliance
+	{2, 17, 0.812, 0.794, "Schiff nach Funkenwasser Hafen", "boat", "Horde", nil}, -- horde
+	{2, 27, 0.068, 0.613, "Schiff nach Theramore", "boat", "Allianz", nil},  -- alliance
+	{2, 26, 0.218, 0.563, "Schiff nach Auberdine", "boat", "Allianz", nil}, -- alliance
+	{2, 23, 0.257, 0.73, "Schiff nach Ratschet", "boat", "Neutral", nil}, -- neutral
+	{2, 30, 0.616, 0.571, "Zeppeline nach Orgrimmar & Grom’gol", "zepp", "Horde", nil}, -- horde
+	{2, 23, 0.312, 0.298, "Zeppeline nach Unterstadt & Orgrimmar", "zepp", "Horde", nil}, -- Horde
+	{2, 38, 0.075, 0.480, "Zeppelin nach Orgrimmar", "zepp", "Horde",  nil}, -- Horde
+	{2, 1, 0.531, 0.047, "Schiff nach Auberdine", "boat", "Allianz", nil}, -- alliance
+	-- Sturmwind Map Markers
+	{2, 26, 0.622, 0.769, "Bank", "bank", "Allianz", nil},
+	{2, 26, 0.617, 0.761, "Mail", "mail", "Allianz", nil},
+	{2, 26, 0.598, 0.752, "Gasthaus", "gh", "Allianz", nil},
+	{2, 26, 0.601, 0.713, "Auktionshaus", "ah", "Allianz", nil},
+	{2, 26, 0.540, 0.545, "Priester Ausbilder", "teacher_class", "Allianz", nil},
+	{2, 26, 0.618, 0.311, "Ingenieurskunst Ausbilder", "teacher_prof", "Allianz", nil},
+}
+
+ModernMapMarkers_Points_en = {
+    -- Kalimdor Dungeons
     {1, 1, 0.123, 0.128, "Blackfathom Deeps", "dungeon", "24-32", 2},
     {1, 23, 0.66, 0.49, "Black Morass", "dungeon", "60", 1},
     {1, 1, 0.51, 0.78, "Crescent Grove", "dungeon", "32-38", 3},
